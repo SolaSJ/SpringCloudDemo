9497bc99614ffaa417c72dbdde5d034aa29a6e8f
solasj <solasj@qq.com>
2018-12-27 00:29:14 +0800

    /actuator/refresh 刷新配置

--- sc-user/src/main/java/com/sola/scuser/controller/UserController.java
+++ sc-user/src/main/java/com/sola/scuser/controller/UserController.java
@@ -2,6 +2,7 @@ package com.sola.scuser.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import java.util.Arrays;
  * @date 2018/12/25
  */
 @Slf4j
+@RefreshScope
 @RestController
 @RequestMapping("/user")
 public class UserController {
