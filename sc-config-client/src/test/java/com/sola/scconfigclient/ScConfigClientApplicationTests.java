b3bfb3877e05c22534b4b49edd70a531281bf4da
SolaSJ <solasj@qq.com>
2018-12-26 18:46:20 +0800

    [add config server]

---
+++ sc-config-client/src/test/java/com/sola/scconfigclient/ScConfigClientApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sola.scconfigclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ScConfigClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
